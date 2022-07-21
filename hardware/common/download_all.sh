bc0fdf41b10600a4cc76ead0654b3fd190cf5c4b
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2022-07-21 07:09:55 +0200

    exploring using spi fread to load data on mch2022 badge (not working yet)

--- hardware/common/download_all.sh
+++ hardware/common/download_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SILICE_URL=https://raw.githubusercontent.com/sylefeb/Silice/draft
 MCH2022_SILICE_URL=https://raw.githubusercontent.com/sylefeb/mch2022-silice/main/
+MCH2022_ICE40_URL=https://raw.githubusercontent.com/badgeteam/mch2022-firmware-ice40/master/
 OPT=-nc
 wget $OPT $SILICE_URL/projects/common/plls/icebrkr_25.v
 wget $OPT $SILICE_URL/projects/common/plls/icebrkr_50.v
@@ -24,4 +25,11 @@ wget $OPT $SILICE_URL/projects/spiflash/spiflash2x.si
 wget $OPT $SILICE_URL/projects/spiflash/spiflash4x.si
 wget $OPT $SILICE_URL/projects/spiflash/ddr_clock.v
 wget $OPT $SILICE_URL/projects/ice-v/CPUs/ice-v-dual.si
+# qpi spram controller for the mch2022 badge
 wget $OPT $MCH2022_SILICE_URL/common/qpsram2x.si
+# for spi_file on the mch2022 badge
+wget $OPT $MCH2022_SILICE_URL/common/spi_file.v
+wget $OPT $MCH2022_ICE40_URL/cores/spi_slave/rtl/spi_dev_core.v
+wget $OPT $MCH2022_ICE40_URL/cores/spi_slave/rtl/spi_dev_proto.v
+wget $OPT $MCH2022_ICE40_URL/cores/spi_slave/rtl/spi_dev_fread.v
+wget $OPT https://raw.githubusercontent.com/no2fpga/no2misc/59350da954e78424117ed01c55b5c7a12e524397/rtl/ram_sdp.v
